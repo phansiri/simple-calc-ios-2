c345f62d1e37583780f0f9c215e2b9ee997b68ee
lit_phansiri <phansiri@uw.edu>
2016-11-01 11:12:59 -0700

    history function works correctly

---
+++ SimpleCalc-iOS/SimpleCalcTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SimpleCalcTableViewCell.swift
+//  SimpleCalc-iOS
+//
+//  Created by Litthideth Phansiri on 11/1/16.
+//  Copyright © 2016 Lit Phansiri. All rights reserved.
+//
+
+import UIKit
+
+class SimpleCalcTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
